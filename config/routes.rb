0ab927008022521d76b9a0785973fc731eab2b78
Julia Waggoner <jjwaggoner27@gmail.com>
2017-02-23 17:08:52 -0500

    implamentation of new header

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'about/show'
+  get 'schedule/about'
 
   root 'home#index'
+
+  get '/redirect', to: 'schedule#redirect', as: 'redirect'
+  get '/callback', to: 'schedule#about', as: 'callback'
+  get '/calendars', to: 'schedule#calendars', as: 'calendars'
+  get '/events/:calendar_id', to: 'schedule#events', as: 'events', calendar_id: /[^\/]+/
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
