ad3171b67a89958e9a576a28fac1c4f9d0ab7f50
Julia Waggoner <jjwaggoner27@gmail.com>
2017-04-30 13:55:51 -0400

    changes to production to make images load

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
