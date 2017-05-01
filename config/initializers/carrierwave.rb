5dc3f0406bf8b5f94b3905fc743a9d0bacb9630b
Julia Waggoner <jjwaggoner27@gmail.com>
2017-05-01 15:39:58 -0400

    AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["KIAIDCGOZS4FWWXT3KQ"],                        # required
-    :aws_secret_access_key  => ENV["EywKOfx8MhVbPeBVvrV5AuYMtCtLoPHvLBNw6ZUJ"]                         # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY"],                        # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_KEY"]                         # required
   }
-  config.fog_directory  = ENV["mediafellow"]                     # required
+  config.fog_directory  = ENV["S3_BUCKET_NAME"]                     # required
 end
